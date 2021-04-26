3b779f19a92bf00c85fb4db2181d9908385f513d
Kartikesh-19 <76430423+Kartikesh-19@users.noreply.github.com>
2021-04-26 11:01:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Page from './component/Page'
+import Reducer from './reducers/Reducer'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
